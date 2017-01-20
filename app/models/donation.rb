f9f89aa7a506a51f2c83d5395c3c0b826c7e9a5d
noahw <noahbweiner@gmail.com>
2017-01-20 09:16:03 -0800

    Write setters for nested donor attributes on Donation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,17 @@
 class Donation < ApplicationRecord
   belongs_to :donor
   belongs_to :category
+
+  delegate :trucking_company, to: :donor, allow_nil: true
+
+  validates :weight, presence: true
+  validates_associated :donor, :category
+
+  def donor=(value)
+    write_attribute :donor_id, Donor.find_or_create_by(name: value).id
+  end
+
+  def trucking_company=(value)
+    donor.update(trucking_company: value)
+  end
 end
