68449d301d486c14bdfc9d9d16bbb3caff5fba63
noahw <noahbweiner@gmail.com>
2017-01-19 19:21:37 -0800

    Set up relationships in model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :donations
+  validates :name, presence: true
 end
--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,2 +1,4 @@
 class Donor < ApplicationRecord
+  has_many :donations
+  validates :name, presence: true
 end
