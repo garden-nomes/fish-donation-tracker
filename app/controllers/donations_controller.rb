ab29973abb36b74e97477991744bfcb4b2797693
noahw <noahbweiner@gmail.com>
2017-01-20 10:02:05 -0800

    Write controller logic for donations#create

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -6,4 +6,22 @@ class DonationsController < ApplicationController
     @categories = Category.all
     @donors = Donor.all
   end
+
+  # POST /donations
+  def create
+    @donation = Donation.new(donation_params)
+
+    if @donation.save
+      flash[:success] = "Donation logged!"
+    else
+      flash[:error] = @donation.errors.full_messages.to_sentence
+    end
+    redirect_to new_donation_path
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:donor, :trucking_company, :category_id, :weight)
+  end
 end
