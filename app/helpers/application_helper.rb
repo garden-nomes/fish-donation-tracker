20cb8c1a42a26ba2a5b55ebbf38405c6f7f42e73
noahw <noahbweiner@gmail.com>
2017-01-20 10:01:49 -0800

    Set up flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bootstrap_alert_class_for(type)
+    case type.to_sym
+    when :success; 'alert-success'
+    when :error;   'alert-danger'
+    when :alert;   'alert-warning'
+    when :notice;  'alert-info'
+    else type.to_s
+    end
+  end
 end
