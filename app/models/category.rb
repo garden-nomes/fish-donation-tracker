2a0e7a0ab0e6c9ad31b3f7c02cdc1b3e24c93f8a
noahw <noahbweiner@gmail.com>
2017-01-23 10:03:21 -0800

    Add scopes to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   has_many :donations
   validates :name, presence: true
+  scope :food, -> { where(food: true) }
+  scope :non_food, -> { where(food: false) }
 
   def to_s
     "#{name}"
