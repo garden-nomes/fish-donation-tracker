f3fe27824d7e164c6c049282c2b11292e0cbc4cb
noahw <noahbweiner@gmail.com>
2017-01-24 10:10:39 -0800

    Implement update/destroy actions and views for donations

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,18 +1,20 @@
 class DonationsController < ApplicationController
+  before_action :set_donation, only: [:update, :edit, :destroy]
+  before_action :set_scopes, only: [:new, :create, :edit, :update]
+
+  # GET /donations
+  def index
+    redirect_to action: 'new'
+  end
+
   # GET /donations/new
   def new
-    @donations = Donation.all
     @donation = Donation.new
-    @categories = Category.all
-    @donors = Donor.all
   end
 
   # POST /donations
   def create
     @donation = Donation.new(donation_params)
-    @donations = Donation.all
-    @categories = Category.all
-    @donors = Donor.all
 
     if @donation.save
       flash[:success] = "Saved!"
@@ -22,7 +24,43 @@ class DonationsController < ApplicationController
     end
   end
 
+  # GET /donations/:id/edit
+  def edit
+  end
+
+  # PATCH /donations/:id
+  def update
+    if @donation.update(donation_params)
+      flash[:success] = "Saved!"
+      redirect_to action: 'new'
+    else
+      flash[:error] = "Unable to update donation."
+      render :edit
+    end
+  end
+
+  # DELETE /donations/:id
+  def destroy
+    if @donation.destroy
+      flash[:success] = "Saved!"
+    else
+      flash[:error] = "Unable to delete donation."
+    end
+
+    redirect_to action: 'new'
+  end
+
   private
+
+  def set_scopes
+    @donations = Donation.all
+    @categories = Category.all
+    @donors = Donor.all
+  end
+
+  def set_donation
+    @donation = Donation.find(params[:id])
+  end
 
   def donation_params
     params.require(:donation).permit(:donor, :trucking_company, :category_id, :weight)
